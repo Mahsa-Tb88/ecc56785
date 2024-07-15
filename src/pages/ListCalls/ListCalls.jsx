2e9b3389ce4f000dc0186324cdc9b938b144fd72
Mahsa Tabesh <50694999+Mahsa-Tb88@users.noreply.github.com>
2024-07-15 13:40:41 -0700

    Update ListCalls.jsx

--- src/pages/ListCalls/ListCalls.jsx
+++ src/pages/ListCalls/ListCalls.jsx
@@ -5,6 +5,8 @@ import "./listCalls.css";
 
 export default function ListCalls() {
   const [listCalls, setListCalls] = useState([]);
+  const [isLoadign, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   //fetch list of calls
   useEffect(() => {
@@ -13,22 +15,59 @@ export default function ListCalls() {
   }, []);
 
   async function fetchAllCalls() {
+    setIsLoading(true);
+    setError(false);
     const result = await getAllCalls();
     if (result) {
       setListCalls(result);
+      setIsLoading(false);
     } else {
-      console.log("result");
+      setError("something went wrong");
     }
   }
+  console.log(listCalls[0]);
+
+  let day, options, month, year;
+  if (listCalls[0]) {
+    const dateOfCall = new Date(listCalls[0].created_at);
+    day = dateOfCall.getDate();
+    options = { month: "long" };
+    month = new Intl.DateTimeFormat("en-US", options).format(dateOfCall);
+    year = dateOfCall.getFullYear();
+  }
+
   return (
-    <div className="py-3 listCalls">
-      {listCalls.map((call) => {
-        return (
-          <div key={call.id}>
-            <Call call={call} />
+    <div>
+      {isLoadign ? (
+        <div className="text-center py-5 text-info fw-semibold">
+          <span>isLoding</span>
+        </div>
+      ) : error ? (
+        <div className="text-center py-5 text-danger fw-semibold">
+          <span>{error}</span>
+        </div>
+      ) : (
+        <div className="py-5 listCalls">
+          <div className="d-flex justify-content-center align-items-center ">
+            <span className="dot"></span>
+            <div className="mx-md-3 mx-1 text ">
+              <span>{month}</span>
+              <span className="ps-1 pe-1">,</span>
+              <span className="me-2">{day}</span>
+              <span>{year}</span>
+            </div>
+            <span className="dot"></span>
           </div>
-        );
-      })}
+          {listCalls.map((call, i) => {
+            let preCall = i > 0 ? listCalls[i - 1] : call;
+            return (
+              <div key={call.id}>
+                <Call call={call} preCall={preCall} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
